e79440a699083a812b6a1a1940f46afa966b1841
TakayukiFutatsugi <jeopardy1216@gmail.com>
2021-04-02 12:25:30 +0000

    semantic ui imported

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,6 +1,7 @@
 import "bootstrap"
 import "bootstrap/scss/bootstrap.scss"
 import '../stylesheets/application'
+
 // This file is automatically compiled by Webpack, along with any other files
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
@@ -14,3 +15,7 @@ import "channels"
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+// Support component names relative to this directory:
+var componentRequireContext = require.context("components", true);
+var ReactRailsUJS = require("react_ujs");
+ReactRailsUJS.useContext(componentRequireContext);
